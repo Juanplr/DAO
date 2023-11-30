9d7b462ab8604a00e4fdf92daaf82d536100d5b9
Soka_jplr <juanlazarorp@gmail.com>
2023-11-30 12:17:51 -0600

    first commit

---
+++ src/main/java/mx/uv/App.java
@@ -0,0 +1,33 @@
+package mx.uv;
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import  com.google.gson.*;
+public class App 
+{
+    static Gson gson = new Gson();
+    static HashMap<String, Usuario> usuarios = new HashMap<>();
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        port(80);
+
+        get("/usuarios", (request, response) ->{
+            response.type("application/json");
+            return gson.toJson(DAO.dameUsuarios());
+        });
+        post("/usuarios", (request, response) ->{
+            String payload = request.body();
+            Usuario u = gson.fromJson(payload, Usuario.class);
+            String id = UUID.randomUUID().toString();
+            u.setId(id);
+            //usuarios.put(id, u);
+            DAO.crearUsuario(u);
+            JsonObject respuesta = new JsonObject();
+            respuesta.addProperty("msj", "");
+            return respuesta;
+        });
+    }
+}
---
+++ src/main/java/mx/uv/Usuario.java
@@ -0,0 +1,38 @@
+package mx.uv;
+
+public class Usuario {
+    String id;
+    String nombre;
+    String password;
+
+    public Usuario(String id, String nombre, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
